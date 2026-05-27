c53edfabc4902e394958b4fb4ec4ef96004e6bee
Gaurish-rana <ranagaurish@gmail.com>
2026-05-27 13:34:31 +0530

    Longest Subbarray

---
+++ Arrays/Easy/Longest_Subarray_with_sum_K.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarrayWithSumK(vector<int> a, long long k)
+{
+    int left = 0, right = 0;
+
+    long long sum = a[0];
+
+    int maxLen = 0;
+
+    int n = a.size();
+
+    while(right < n)
+    {
+        while(left <= right && sum > k)
+        {
+            sum -= a[left];
+            left++;
+        }
+
+        if(sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        right++;
+
+        if(right < n)
+        {
+            sum += a[right];
+        }
+    }
+
+    return maxLen;
+}
+
+int main()
+{
+    vector<int> a = {1,2,1,1,1,3,2};
+    long long k = 3;
+
+    cout << "Longest Length: " 
+         << longestSubarrayWithSumK(a, k);
+
+    return 0;
+}
