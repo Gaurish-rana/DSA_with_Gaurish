4f6839fab6eacf6ad049ab0cc294a4e36ab3a186
Gaurish-rana <ranagaurish@gmail.com>
2026-05-31 11:55:09 +0530

    Done 1 array question

---
+++ Arrays/Easy/Running_Sum_of_1d_Array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> runningSum(vector<int>& nums) {
+    for (int i = 1; i < nums.size(); i++) {
+        nums[i] += nums[i - 1];
+    }
+    return nums;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
+
+    vector<int> result = runningSum(nums);
+
+    cout << "Running Sum Array: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
