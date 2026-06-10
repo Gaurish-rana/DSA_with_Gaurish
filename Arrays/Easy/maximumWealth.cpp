c4f597590983f3462224eb8ac6dffe44e11924bf
Gaurish-rana <ranagaurish@gmail.com>
2026-06-10 20:30:53 +0530

    Done with maximum wealth

---
+++ Arrays/Easy/maximumWealth.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int m = accounts.size();
+        int n = accounts[0].size();
+
+        int maxWealth = 0;
+
+        for (int i = 0; i < m; i++) {
+            int wealth = 0;
+
+            for (int j = 0; j < n; j++) {
+                wealth += accounts[i][j];
+            }
+
+            if (wealth > maxWealth) {
+                maxWealth = wealth;
+            }
+        }
+
+        return maxWealth;
+    }
+};
+
+int main() {
+    vector<vector<int>> accounts = {
+        {1, 2, 3},
+        {3, 2, 1},
+        {4, 5, 6}
+    };
+
+    Solution obj;
+    cout << "Maximum Wealth: " << obj.maximumWealth(accounts) << endl;
+
+    return 0;
+}
