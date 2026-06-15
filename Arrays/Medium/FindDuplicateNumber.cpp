85ebd68728c01b39a42b19b88cf48cdc1c7e618d
Gaurish-rana <ranagaurish@gmail.com>
2026-06-15 14:23:09 +0530

    Done with Find DuplicateNumber

---
+++ Arrays/Medium/FindDuplicateNumber.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Detect the cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance of the cycle
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 3, 4, 2, 2};
+
+    Solution obj;
+    cout << "Duplicate Number: " << obj.findDuplicate(nums) << endl;
+
+    return 0;
+}
